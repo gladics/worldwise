82b2ee38b59ebc576abce8058623442deacce4eb
Gabor Ladics <gladics87@gmail.com>
2023-12-28 11:17:17 +0100

    [WorldWise]: create css files, create jsx files for AppNav and PageNave components

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+
 function HomePage() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>Home</h1>
+      <Link to="/app">Go to the app</Link>
+    </div>
+  );
 }
 
 export default HomePage;
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,5 +1,11 @@
+import PageNav from "../components/PageNav";
+
 function PageNotFound() {
-  return <div>Page not found!</div>;
+  return (
+    <div>
+      <PageNav /> Page not found!
+    </div>
+  );
 }
 
 export default PageNotFound;
