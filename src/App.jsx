6ec5b00daeed1557f52cdbc437f3078b8c6438d9
Gabor Ladics <gladics87@gmail.com>
2023-12-28 13:15:43 +0100

    [WorldWise]: create jsx files for CityList, Map, Sidebar components, create nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,22 @@ import HomePage from "./pages/HomePage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />}></Route>
+        <Route index element={<HomePage />}></Route>
         <Route path="product" element={<Product />}></Route>
         <Route path="pricing" element={<Pricing />}></Route>
         <Route path="login" element={<Login />}></Route>
-        <Route path="app" element={<AppLayout />}></Route>
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<CityList />} />
+          <Route path="cities" element={<CityList />} />
+          <Route path="countries" element={<p>List of countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />}></Route>
       </Routes>
     </BrowserRouter>
